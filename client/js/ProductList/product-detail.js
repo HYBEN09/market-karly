1aeef3d40c715df3c79cab1f9d60a46891edc18f
hyebeen byun <hyebeen2658@naver.com>
2023-08-08 10:08:40 +0900

    [feat] WHY KARLY section 동적 생성

---
+++ client/js/ProductList/product-detail.js
@@ -0,0 +1,23 @@
+import { handlerSetText } from "../sign/index.js";
+import { productDetails } from "../data/productDetail.js";
+
+const productDetailElement = document.querySelector(".product-detail__reasons");
+
+// WHY KARLY
+function renderProductDetails() {
+  if (productDetailElement) {
+    productDetails.forEach((detail) => {
+      productDetailElement.innerHTML += `
+          <dl>
+            <dt>
+              <img src="${detail.imageUrl}" alt="" class="why-karly_img">
+              ${detail.title}
+            </dt>
+            <dd>${detail.description}</dd>
+          </dl>
+        `;
+    });
+  }
+}
+
+renderProductDetails();
---
+++ client/js/data/productDetail.js
@@ -0,0 +1,32 @@
+export const productDetails = [
+  {
+    title: "깐깐한 상품위원회",
+    description:
+      "나와 내 가족이 먹고 쓸 상품을 고르는 마음으로 매주 상품을 직접 먹어보고, 경험해보고 성분, 맛, 안정성 등 다각도의 기준을 통과한 상품만을 판매합니다.",
+    imageUrl: "https://res.kurly.com/pc/ico/1910/01_check.svg",
+  },
+  {
+    title: "차별화된 Karly Only 상품",
+    description:
+      "전국 각지와 해외의 훌륭한 생산자가 믿고 선택하는 파트너, 컬리. 3천여 개가 넘는 컬리 단독 브랜드, 스펙의 Karly Only 상품을 믿고 만나보세요.",
+    imageUrl: "https://res.kurly.com/pc/ico/1910/02_only.svg",
+  },
+  {
+    title: "신선한 풀콜드체인 배송",
+    description:
+      "온라인 업계 최초로 산지에서 문 앞까지 상온, 냉장, 냉동 상품을 분리 포장 후 최적의 온도를 유지하는 냉장 배송 시스템, 풀콜드체인으로 상품을 신선하게 전해드립니다.",
+    imageUrl: "https://res.kurly.com/pc/ico/1910/03_cold.svg",
+  },
+  {
+    title: "고객, 생산자를 위한 최선의 가격",
+    description:
+      "매주 대형 마트와 주요 온라인 마트의 가격 변동 상황을 확인해 신선식품은 품질을 타협하지 않는 선에서 최선의 가격으로, 가공식품은 언제나 합리적인 가격으로 정기 조정합니다.",
+    imageUrl: "https://res.kurly.com/pc/ico/1910/04_price.svg",
+  },
+  {
+    title: "환경을 생각하는 지속 가능한 유통",
+    description:
+      "친환경 포장재부터 생산자가 상품에만 집중할 수 있는 직매입 유통구조까지, 지속 가능한 유통을 고민하며 컬리를 있게 하는 모든 환경(생산자, 커뮤니티, 직원)이 더 나아질 수 있도록 노력합니다.",
+    imageUrl: "https://res.kurly.com/pc/ico/1910/05_eco.svg",
+  },
+];
